4fe41c9d2fd29981eed6978e784ebbd27ca0fa73
gmsantos <gabriel.ms1@hotmail.com>
2020-02-19 11:43:04 -0300

    move seed to migration

--- Migrations/20200219144053_SeedData.Designer.cs
+++ Migrations/20200219144053_SeedData.Designer.cs
@@ -4,14 +4,16 @@ using AspNetMvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetMvc.Migrations
 {
     [DbContext(typeof(RdsContext))]
-    partial class RdsContextModelSnapshot : ModelSnapshot
+    [Migration("20200219144053_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
